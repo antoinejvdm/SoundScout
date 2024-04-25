9f0ff28261b74457f3173afe78ffc053e9afd4a8
Antoine Janssens <antcool.jeux@gmail.com>
2024-04-25 17:37:30 +0200

    last audio

--- exampelNodebook.py
+++ exampelNodebook.py
@@ -4,16 +4,16 @@ from scipy.io import wavfile
 from IPython.display import Audio
 import pyroomacoustics as pra
 
-corners = np.array([[0,0], [5,0], [5,3], [0,3]]).T  # [x,y]
+corners = np.array([[0,0], [10,0], [10,6], [0,6]]).T  # [x,y]
 room_a = pra.Room.from_corners(corners)
 
 # specify signal source
 fs, signal = wavfile.read("Coldplay - Viva La Vida (short).wav")
 mono_signal = np.sum(signal, axis=1 )
-absorbption = 0.4
+absorbption = 0.40
 
 room = pra.Room.from_corners(corners, fs=fs, max_order=3, materials=pra.Material(absorbption, 0.15), ray_tracing=True, air_absorption=True)
-room.extrude(2., materials=pra.Material(absorbption, 0.15))
+room.extrude(4., materials=pra.Material(absorbption, 0.15))
 
 
 
@@ -23,15 +23,15 @@ room.set_ray_tracing(receiver_radius=0.5, n_rays=10000, energy_thres=1e-5)
 
 
 # add source and set the signal to WAV file content
-room.add_source([1, 1.5, 0.75], signal=mono_signal)
+room.add_source([2, 3, 2], signal=mono_signal)
 
 # add two-microphone array
 num_mics = 4
 radius = 0.21  # Radius of the circle
 theta = np.linspace(0, 2 * np.pi, num_mics, endpoint=False)
-x_origin = 4.0  # New x-origin
-y_origin = 1.5  # New y-origin
-z_origin = 0.75
+x_origin = 8  # New x-origin
+y_origin = 3  # New y-origin
+z_origin = 2
 mic_positions = np.array([radius * np.cos(theta) + x_origin, radius * np.sin(theta) + y_origin, np.ones(num_mics) * z_origin])
 room.add_microphone(mic_positions)
 
