c6a1f5f3a69fe1c06fee0e0a565653bedd020601
Antoine Janssens <antcool.jeux@gmail.com>
2024-05-16 14:11:24 +0200

    only nesesary code

--- LC-SRP-python-main/main.py
+++ LC-SRP-python-main/main.py
@@ -57,8 +57,6 @@ tmp = scipy.io.loadmat('coord_loc_1_8.mat');
 true_loc = tmp.get('true_loc');
 # compute ground truth DOA vectors for source locations
 
-from DOA import DOA
-true_DOAvec, DOAang=DOA(true_loc,arrayCenterPos)
 L = 32;
 
 
@@ -95,18 +93,6 @@ T, N_mm  =calc_sampleParam(micPos, w_0, c);
 N_aux = range(0,3);
 
 ## PROCESSING
-
-# init results (per source location, frame, number of auxilary samples)
-# approximation error in dB
-
-
-# approxErr_dB=np.zeros((len(true_loc), L, len(N_aux)))
-# locErr=np.zeros((len(true_loc), L, len(N_aux)+1))
-
-# res = {'field1': approxErr_dB, 'field2': locErr};
-
-
-
 import soundfile as sf
 
 for true_loc_idx in range (1,len(true_loc)+1):
@@ -116,35 +102,10 @@ for true_loc_idx in range (1,len(true_loc)+1):
     #GENERATE MICROPHONE SIGNALS
     #speech componentfor selected source
     x_TD,samplerate = sf.read('x_loc' +str(true_loc_idx)+ '.wav');
-    #noise component
-    # v_TD,sr = sf.read('v.wav');
-    #scale noise component
-    from set_SNR import set_SNR
-    # v_TD = set_SNR(x_TD, v_TD, SNR);
 
     # transform to STFT domain
     from calc_STFT import calc_STFT
     x_STFT,f_x = calc_STFT(x_TD, fs, win, N_STFT, R_STFT, 'onesided');
-    # v_STFT,f_v = calc_STFT(v_TD, fs, win, N_STFT, R_STFT, 'onesided');
-
-    # # discard frames that do not contain speech energy(local SNR 15 dB below average)
-    # l = 1;
-    # useframe_idx = np.array([]);
-    # while len(useframe_idx) < L:
-    #     SNR_local = 10*math.log(((sum(np.power(abs(x_STFT[:, l-1, 1-1]), 2)) / sum(np.power(abs(v_STFT[:, l-1, 1-1]), 2)))),10);
-    #     if SNR_local > SNR - 15:
-    #         useframe_idx=np.append(useframe_idx, l, axis=None)
-    #     l = l + 1;
-
-
-    # # final microphone signal in STFT domain
-    # len_frame=len(useframe_idx)
-    # y_STFT = x_STFT[:, range(0, len_frame), :] + v_STFT[:, range(0, len_frame), :];
-    # for i in range(1, len_frame + 1):
-    #     y_STFT[:, i - 1, :] = x_STFT[:, int(useframe_idx[i - 1]) - 1, :] + v_STFT[:, int(useframe_idx[i - 1]) - 1,
-    #                                                                             :];
-
-
 
     ## PROCESSING
     from calc_FD_GCC import calc_FD_GCC
@@ -196,43 +157,5 @@ for true_loc_idx in range (1,len(true_loc)+1):
     plt.show()
         
     print('done')
-
-
-    # # SRP approximation based on shannon nyquist sampes
-    # print('* compute SRP approximation...')
-    # t = time.time();
-    # from calc_SRP import calc_SRP
-    # SRP_appr = calc_SRP(psi_STFT, omega, T, N_mm, N_aux, Delta_t_i);
-    # elapsed = time.time() - t;
-    # print(elapsed)
-    # print('done')
-
-    ####
-
-    # approxErr_dB = np.zeros([L, len(N_aux)]);
-    # locErr = np.zeros([L, len(N_aux) + 1]);
-
-    # maxIdx_conv = np.argmax(SRP_conv, 1);
-    # maxIdx_conv = maxIdx_conv.reshape(-1,1);
-    # estim_DOAvec = DOAvec_i_tmp[0:len(maxIdx_conv),:];
-    # for i in range (1,len(maxIdx_conv)+1):
-    #     estim_DOAvec[i-1] = DOAvec_i_tmp[maxIdx_conv[i-1],:];
-    # locErr[:, 0] = np.rad2deg(np.arccos(np.dot(estim_DOAvec , np.transpose(true_DOAvec[true_loc_idx-1,:]))/(np.sqrt(np.sum(np.power(estim_DOAvec,2), axis=1)) * LA.norm(true_DOAvec[true_loc_idx-1,:]))));
-
-
-    # for N_aux_ind in range (1,len(N_aux)+1):
-    #     approxErr = np.sum(np.power(SRP_conv - SRP_appr[:,:, N_aux_ind-1], 2), axis=1) / np.sum(np.power(SRP_conv, 2), axis=1);
-    #     approxErr_dB[:, N_aux_ind-1] = 10*np.log10(approxErr);
-
-    #     maxIdx_conv = np.argmax(SRP_appr[:,:, N_aux_ind-1], 1);
-    #     estim_DOAvec = DOAvec_i_tmp2[0:len(maxIdx_conv),:];
-    #     for i in range(1, len(maxIdx_conv) + 1):
-    #         estim_DOAvec[i - 1] = DOAvec_i_tmp2[maxIdx_conv[i - 1], :];
-    #     locErr[:, N_aux_ind] = np.rad2deg(np.arccos(np.dot(estim_DOAvec , np.transpose(true_DOAvec[true_loc_idx-1,:]))/(np.sqrt(np.sum(np.power(estim_DOAvec,2), axis=1)) * LA.norm(true_DOAvec[true_loc_idx-1,:]))));
-
-
-    # res['field1'][true_loc_idx-1,:,:] = approxErr_dB;
-    # res['field2'][true_loc_idx-1,:,:] = locErr;
-
 print('DONE.')
 
